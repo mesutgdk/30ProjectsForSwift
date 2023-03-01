299b0d3b4aa43bd6941d1a9d3f038cbcb4fe52b5
mesut <112901255+mesutgdk@users.noreply.github.com>
2023-03-01 05:28:21 -0800

    Add files via upload

--- simpleTimer/SimpleTimer/ViewController.swift
+++ simpleTimer/SimpleTimer/ViewController.swift
@@ -9,29 +9,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var timer: UILabel!
+    @IBOutlet weak var timerLabel: UILabel!
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        let startButtonPressed = UIButton(type: .custom)
-        startButtonPressed.setImage(UIImage(named: "play.png"), for: UIControl.State.normal)
-        
-        let stopButtonPressed = UIButton(type: .custom)
-        stopButtonPressed.setImage(UIImage(named: "pause.png"), for: UIControl.State.normal)
-        // Do any additional setup after loading the view.
-    }
+    
+    var isPause = true
+    var count : Double = 0
+    var timer : Timer = Timer()
+    var countString : String = ""
 
-    @IBAction func resetButtonPressed(_ sender: UIButton) {
+
+
+    @IBAction func playButtonPressed(_ sender: UIButton) {
+        if isPause {
+            timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(ViewController.update), userInfo: nil, repeats: true)
+            isPause = false
     }
-    
-    @IBAction func startButtonPressed(_ sender: UIButton) {
-    }
-    
-    @IBAction func stopButtonPressed(_ sender: UIButton) {
-    }
-    
-   
-    
 }
+    @IBAction func pauseButtonPressed(_ sender: UIButton) {
+        if !isPause {
+            timer.invalidate()
+            isPause = true
+           
+        }
+    }
+    
+    @IBAction func resetButtonPressed(_ sender: UIButton) {
+        if count > 0 {
+        count = 0
+        timer.invalidate()
+        timerLabel.text = "0.0"
+        isPause = true
+        }
+    }
+    
+    @ objc func update () -> Void {
+        count = count + 0.1
+        
+        countString = ("\(round(count*10)/10)")
+        timerLabel.text = countString
+    }
+    
+    }
+
 
