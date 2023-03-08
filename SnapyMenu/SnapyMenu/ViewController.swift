acf33f65595ee3dc53855f3c42b2f468dbfd92ef
mesut <112901255+mesutgdk@users.noreply.github.com>
2023-03-08 18:35:49 +0300

    Add files via upload

---
+++ SnapyMenu/SnapyMenu/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  SnapyMenu
+//
+//  Created by Mesut Gedik on 7.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    
+    
+}
+
