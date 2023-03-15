0ed75a6883b1ebd33ddb58501dfc83c897ce38ef
mesut <112901255+mesutgdk@users.noreply.github.com>
2023-03-15 08:51:42 +0300

    Add files via upload

---
+++ 5-CarouselEffect/5-CarouselEffect/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  5-CarouselEffect
+//
+//  Created by Mesut Gedik on 15.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
