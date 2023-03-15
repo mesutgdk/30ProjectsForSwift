69d683f0857c08be035e514766e42638bade4521
mesut <112901255+mesutgdk@users.noreply.github.com>
2023-03-15 10:45:32 +0300

    Add files via upload

---
+++ 4-SnapyMenu/SnapyMenu/Constraits.swift
@@ -0,0 +1,14 @@
+//
+//  Constraits.swift
+//  SnapyMenu
+//
+//  Created by Mesut Gedik on 10.03.2023.
+//
+
+import Foundation
+
+struct Constraits {
+    static let FV : String = "FirstView"
+    static let SV : String = "SecondView"
+    static let TV : String = "ThirdView"
+}
