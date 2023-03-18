65cdb7b190b1278c11549b51bbc928851a602e8d
mesut <112901255+mesutgdk@users.noreply.github.com>
2023-03-18 23:25:40 +0300

    Add files via upload

--- 7-PullToRefresh/7-PullToRefresh/RefreshViewController.swift
+++ 7-PullToRefresh/7-PullToRefresh/RefreshViewController.swift
@@ -11,11 +11,29 @@ class RefreshViewController: UITableViewController {
 
     let emojiArray : Array = ["😄","😀","😆","😁","😆","🥹","😂","🤣","🥲","😊","😇","☺️","🙃","😉","😍","😘","😗","😙","😋","😝","😜","🤪","🤨","🧐","🤓","😎","🤩","🤩","😏","😞","😩","😕","😭","😢","🥺","😡","🤬","🤯","🥶","🥵","😱","🤥","🫡","🙄","🥱","😴","🤢","🤮","🤧","🤑","😷","👻","☠️","😾","🙀","👩🏽‍🚒","🧝‍♂️","👯‍♀️","👩🏻‍🦼","🚶🏽‍♂️","🧍🏼‍♂️","👼🏿","🤴"]
     
+    let cell:String = "cell"
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view.
     }
 
-
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cell, for: indexPath)
+        let n1 = emojiArray.count
+        let n = Int.random(in: 0 ... n1-1)
+        let smiley = "\(emojiArray[n]) \(emojiArray[n]) \(emojiArray[n]) \(emojiArray[n]) \(emojiArray[n])"
+        cell.textLabel?.text = smiley
+        cell.textLabel?.font = UIFont.systemFont(ofSize: 40)
+        return cell
+    }
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return  100.0
+    }
 }
 
