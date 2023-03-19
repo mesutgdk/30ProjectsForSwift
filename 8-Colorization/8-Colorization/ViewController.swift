ed8aa00bca6980e994bbef2f2cf4683774439b9a
mesut <112901255+mesutgdk@users.noreply.github.com>
2023-03-19 22:49:23 +0300

    Add files via upload
    code is ended, now visual is started

--- 8-Colorization/8-Colorization/ViewController.swift
+++ 8-Colorization/8-Colorization/ViewController.swift
@@ -6,14 +6,45 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    var player : AVAudioPlayer!
+    var sleepSong : String = "ninni"
+    var songRestart : Bool = false
+    var songPaused : Bool = true
+    
+    @IBOutlet weak var playButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        playButton.setImage(UIImage(named: "playButton.jpg"), for: UIControl.State.normal)
     }
 
+    @IBAction func playButton(_ sender: UIButton) {
+    playSound(soundName: sleepSong)
+    }
+    
+    func playSound(soundName: String) {
+        if songPaused == true {
+            let url = Bundle.main.url(forResource: soundName, withExtension: ".mp3")
+            player = try! AVAudioPlayer(contentsOf: url!)
+            player.play()
+            songPaused = false
+           // print("song is started \(!songPaused)")
 
+        } else if songRestart == false, songPaused == false{
+            player.pause()
+            songRestart = true
+         //   print("song is paused \(!songPaused)")
+        } else if songRestart == true{
+            player.play(atTime: player.deviceCurrentTime)
+//            print("restarted at \(String(describing: player.deviceCurrentTime + 0.01 ))")
+            songRestart = false
+        } else {
+            print ("nothing to do")
+        }
+                
+    }
 }
 
