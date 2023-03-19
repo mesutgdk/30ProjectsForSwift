77b2ba5e883206043315342720ce8f313bbdd201
mesut <112901255+mesutgdk@users.noreply.github.com>
2023-03-19 18:04:02 +0300

    Add files via upload

---
+++ 8-Colorization/8-Colorization/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  8-Colorization
+//
+//  Created by Mesut Gedik on 19.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
