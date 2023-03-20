01ef9fc63d0040201af39b8e36fc3d9f9fef7ac4
mesut <112901255+mesutgdk@users.noreply.github.com>
2023-03-20 16:04:03 +0300

    Add files via upload

--- 8-Colorization/8-Colorization/ViewController.swift
+++ 8-Colorization/8-Colorization/ViewController.swift
@@ -12,8 +12,7 @@ class ViewController: UIViewController {
     
     @IBOutlet var backgroundImage: UIImageView!
     
-    let gradientLayer = CAGradientLayer()
-    
+    var gradientLayer: CAGradientLayer?
     
     var player : AVAudioPlayer!
     
@@ -23,19 +22,6 @@ class ViewController: UIViewController {
     
     var songPaused : Bool = true
     
-    var backgroundColor: (red: CGFloat, green: CGFloat,blue: CGFloat,alpha: CGFloat)! {
-        didSet {
-            let color1 = UIColor(red: backgroundColor.blue,
-                                 green: backgroundColor.green,
-                                 blue: 0,
-                                 alpha: backgroundColor.alpha).cgColor
-            let color2 = UIColor(red: backgroundColor.red,
-                                 green: backgroundColor.green,
-                                 blue: backgroundColor.blue,
-                                 alpha: backgroundColor.alpha).cgColor
-            gradientLayer.colors = [color1, color2]
-        }
-    }
     
     var timer : Timer?
     
@@ -44,10 +30,14 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         playButton.setImage(UIImage(named: "playButton.jpg"), for: UIControl.State.normal)
-    
+        gradientLayer = CAGradientLayer()
+        gradientLayer?.colors = [UIColor.red.cgColor, UIColor.yellow.cgColor]
+        gradientLayer?.startPoint = CGPoint(x: 0.0, y: 0.0)
+        gradientLayer?.endPoint = CGPoint(x: 1.0, y: 1.0)
+        gradientLayer?.frame = view.bounds
+        view.layer.insertSublayer(gradientLayer!, at: 0)
     }
     override func viewWillAppear(_ animated: Bool) {
-       randomColor()
     }
 
     @IBAction func playButton(_ sender: UIButton) {
@@ -57,27 +47,21 @@ class ViewController: UIViewController {
     func playSound(soundName: String) {
         if songPaused == true {
             let url = Bundle.main.url(forResource: soundName, withExtension: ".mp3")
-            player = try! AVAudioPlayer(contentsOf: url!)
-            player.play()
-            songPaused = false
-            if (timer == nil) {
-                Timer.scheduledTimer(timeInterval: 0.3, target: self, selector: #selector(ViewController.randomColor), userInfo: nil, repeats: true)
+            do{
+                player = try AVAudioPlayer(contentsOf: url!)
+                player.play()
+            } catch {
+                print("Error playing audio: \(error.localizedDescription)")
             }
+            songPaused = false
+            let animation = CABasicAnimation(keyPath: "colors")
+            animation.fromValue = [UIColor.red.cgColor, UIColor.yellow.cgColor]
+            animation.toValue = [UIColor.blue.cgColor, UIColor.green.cgColor]
+            animation.autoreverses = true
+            animation.duration = 1.0
+            animation.repeatCount = .infinity
+            gradientLayer?.add(animation, forKey: "gradientAnimation")
             
-            let redValue = CGFloat(drand48())
-            let blueValue =  CGFloat(drand48())
-            let greenValue = CGFloat(drand48())
-            
-            self.view.backgroundColor = UIColor(red: redValue, green: greenValue, blue: blueValue, alpha: 1.0)
-            
-            //graditent color layer
-            gradientLayer.frame = view.bounds
-            gradientLayer.startPoint = CGPoint(x: 0, y: 0)
-            gradientLayer.endPoint = CGPoint(x: 1, y: 1)
-          
-            self.view.layer.insertSublayer(gradientLayer, at: 0)
-           // print("song is started \(!songPaused)")
-
         } else if songRestart == false, songPaused == false{
             player.pause()
             songRestart = true
@@ -93,16 +77,6 @@ class ViewController: UIViewController {
        
         
     }
-    
-    @objc func randomColor() {
-        
-        let redValue = CGFloat(drand48())
-        let blueValue =  CGFloat(drand48())
-        let greenValue = CGFloat(drand48())
-        
-        
-        backgroundColor = (redValue, blueValue, greenValue, 1)
-        
-    }
+   
 }
   
