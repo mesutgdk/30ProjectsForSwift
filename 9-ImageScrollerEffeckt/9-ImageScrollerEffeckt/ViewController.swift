d610d66030e358ba9c3d633ba6dd93afcb8efb95
mesut <112901255+mesutgdk@users.noreply.github.com>
2023-03-20 11:57:52 +0300

    Add files via upload
    new day- new project- A beautiful start

---
+++ 9-ImageScrollerEffeckt/9-ImageScrollerEffeckt/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  9-ImageScrollerEffeckt
+//
+//  Created by Mesut Gedik on 20.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
