6f468790524cf3edaf0fe484d61cdce7657d7713
mesut <112901255+mesutgdk@users.noreply.github.com>
2023-03-22 02:13:23 +0300

    Add files via upload

---
+++ 10-VideoBackground/10-VideoBackground/LoginScreenVC.swift
@@ -0,0 +1,29 @@
+//
+//  LoginScreenVC.swift
+//  10-VideoBackground
+//
+//  Created by Mesut Gedik on 22.03.2023.
+//
+
+import UIKit
+
+class LoginScreenVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ 10-VideoBackground/10-VideoBackground/WellComeScreenVC.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  10-VideoBackground
+//
+//  Created by Mesut Gedik on 21.03.2023.
+//
+
+import UIKit
+
+class WellComeScreenVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
