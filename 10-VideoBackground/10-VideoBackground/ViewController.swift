6e931f425cd4a082d3fd65831e73bcbdfdab256b
mesut <112901255+mesutgdk@users.noreply.github.com>
2023-03-21 15:36:50 +0300

    Add files via upload

---
+++ 10-VideoBackground/10-VideoBackground/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  10-VideoBackground
+//
+//  Created by Mesut Gedik on 21.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
