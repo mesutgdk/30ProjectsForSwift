f0feb1bd06024babf046f4b49593b924dac008e1
Mesut GEDİK <112901255+mesutgdk@users.noreply.github.com>
2023-03-27 15:52:52 +0300

    Add files via upload

--- 14-SlotMachine/14-SlotMachine/SlotViewController.swift
+++ 14-SlotMachine/14-SlotMachine/SlotViewController.swift
@@ -7,22 +7,38 @@
 
 import UIKit
 
-class SlotViewController: UIViewController {
+class SlotViewController: UIViewController, UIPickerViewDataSource {
+    
+    let slotArray: [String] = ["🫵","🫦","🖕","🎃","💩","🍋","🍒","🍬"]
 
     @IBOutlet weak var appImage: UIImageView!
     @IBOutlet weak var backgroundImage: UIImageView!
-    
     @IBOutlet weak var slotPickerView: UIPickerView!
-    
-
     @IBOutlet weak var prizeLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        slotPickerView.delegate = self
+        slotPickerView.dataSource = self
+        prizeLabel.text = ""
+        
     }
 
     @IBAction func turnButtonPressed(_ sender: UIButton) {
     }
     
 }
-
+extension SlotViewController: UIPickerViewDelegate {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        3
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return slotArray.count
+    }
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return slotArray[row]
+    }
+    
+    
+}
