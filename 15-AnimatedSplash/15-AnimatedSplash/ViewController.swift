019fe4d91f30701bbd6888e728e1ad63aa3cdde3
Mesut GEDİK <112901255+mesutgdk@users.noreply.github.com>
2023-03-28 12:08:35 +0300

    Add files via upload

---
+++ 15-AnimatedSplash/15-AnimatedSplash/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  15-AnimatedSplash
+//
+//  Created by Mesut Gedik on 28.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
