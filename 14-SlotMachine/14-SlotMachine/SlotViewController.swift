35e5e3014814d94896a9292cc899d4a2c58d2d4b
Mesut GEDİK <112901255+mesutgdk@users.noreply.github.com>
2023-03-28 08:45:29 +0300

    Add files via upload

--- 14-SlotMachine/14-SlotMachine/SlotViewController.swift
+++ 14-SlotMachine/14-SlotMachine/SlotViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class SlotViewController: UIViewController, UIPickerViewDataSource {
     
-    let slotArray: [String] = ["🫵","🫦","🖕","🎃","💩","🍋","🍒","🍬"]
+    let slotArray: [String] = ["🍬","🫦","🖕","🎃","💩","🍋","🍒","🫵"]
 
     @IBOutlet weak var appImage: UIImageView!
     @IBOutlet weak var backgroundImage: UIImageView!
@@ -21,11 +21,25 @@ class SlotViewController: UIViewController, UIPickerViewDataSource {
         slotPickerView.delegate = self
         slotPickerView.dataSource = self
         prizeLabel.text = ""
-        
     }
 
-    @IBAction func turnButtonPressed(_ sender: UIButton) {
+    @IBAction func goButtonPressed(_ sender: UIButton) {
+        
+        var randomNum : Int {
+         return Int.random(in: 0...8)
+        }
+        slotPickerView.selectRow(randomNum, inComponent: 0, animated: true)
+        slotPickerView.selectRow(randomNum, inComponent: 1, animated: true)
+        slotPickerView.selectRow(randomNum, inComponent: 2, animated: true)
+        let firstSlot = slotPickerView.selectedRow(inComponent: 0)
+        let secondSlot = slotPickerView.selectedRow(inComponent: 1)
+        let thirdSlot = slotPickerView.selectedRow(inComponent: 2)
+       // print("\(firstSlot)\(secondSlot)\(thirdSlot)")
+        if firstSlot != secondSlot {
+            
+        }
     }
+    
     
 }
 extension SlotViewController: UIPickerViewDelegate {
@@ -36,8 +50,28 @@ extension SlotViewController: UIPickerViewDelegate {
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
         return slotArray.count
     }
+    
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
         return slotArray[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat {
+        return 60.0
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, widthForComponent component: Int) -> CGFloat {
+        60.0
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
+        let pickerLabel = UILabel()
+        let titleData = slotArray[row]
+        let myTitle = NSAttributedString(string: titleData, attributes: [NSAttributedString.Key.font:UIFont(name: "Georgia", size: 26.0)!,NSAttributedString.Key.foregroundColor:UIColor.black])
+        pickerLabel.attributedText = myTitle
+        let hue = CGFloat(row)/CGFloat(slotArray.count)
+        pickerLabel.backgroundColor = UIColor(hue: hue, saturation: 1.0, brightness:1.0, alpha: 1.0)
+        pickerLabel.textAlignment = .center
+        return pickerLabel
     }
     
     
