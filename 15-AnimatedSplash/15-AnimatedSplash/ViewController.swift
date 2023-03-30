a4026e7fd8c9fd34a17159dbc42b0cab391d1c0a
Mesut GEDİK <112901255+mesutgdk@users.noreply.github.com>
2023-03-30 11:52:45 +0300

    Add files via upload
    new view is added, now app logo is shown

--- 15-AnimatedSplash/15-AnimatedSplash/ViewController.swift
+++ 15-AnimatedSplash/15-AnimatedSplash/ViewController.swift
@@ -9,9 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private let imageView: UIImageView = {
+        let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 150, height: 150))
+        imageView.image = UIImage(named: "Eksi")
+        return imageView
+    } ()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.addSubview(imageView)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        imageView.center = view.center
     }
 
 
