ff33c6f21aaace1e7c5afb2cb70d2098f63c74eb
Mesut GEDİK <112901255+mesutgdk@users.noreply.github.com>
2023-03-30 13:27:21 +0300

    Add files via upload

---
+++ 15-AnimatedSplash/15-AnimatedSplash/AnimatedViewController.swift
@@ -0,0 +1,60 @@
+//
+//  ViewController.swift
+//  15-AnimatedSplash
+//
+//  Created by Mesut Gedik on 28.03.2023.
+//
+
+import UIKit
+
+class AnimatedViewController: UIViewController {
+
+    private let imageView: UIImageView = {
+        let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 150, height: 150))
+        imageView.image = UIImage(named: "Eksi")
+        return imageView
+    } ()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.addSubview(imageView)
+//        view.backgroundColor = .link
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        imageView.center = view.center
+
+        DispatchQueue.main.asyncAfter(deadline: .now()+0.5, execute: {
+            self.animate()
+        })
+    }
+    
+    private func animate() {
+        UIView.animate(withDuration: 1) {
+            let size = self.view.frame.size.width * 4
+            let diffX = size - self.view.frame.size.width
+            let diffY = self.view.frame.size.height - size
+            
+            self.imageView.frame = CGRect(
+                x: -(diffX/2),
+                y: diffY/2,
+                width: size,
+                height: size
+            )
+            //self.imageView.alpha = 0
+        }
+        UIView.animate(withDuration: 1.5) {
+            self.imageView.alpha = 0
+        } completion: { finished in
+            if finished {
+                let viewController = HomeViewController()
+                viewController.modalTransitionStyle = .crossDissolve
+                viewController.modalPresentationStyle = .fullScreen
+                self.present(viewController, animated: true)
+            }
+        }
+
+    }
+
+}
+
---
+++ 15-AnimatedSplash/15-AnimatedSplash/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  HomeViewController.swift
+//  15-AnimatedSplash
+//
+//  Created by Mesut Gedik on 30.03.2023.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .link
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
