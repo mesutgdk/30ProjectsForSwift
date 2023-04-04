37bf9f84ccee61e1eed9127b701457aa9f17a408
Mesut GEDİK <112901255+mesutgdk@users.noreply.github.com>
2023-04-04 17:43:20 +0300

    Add files via upload

---
+++ 18-LimitTheCharacter/18-LimitTheCharacter/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  18-LimitTheCharacter
+//
+//  Created by Mesut Gedik on 4.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var textView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func backButtonPressed(_ sender: UIButton) {
+    }
+    @IBAction func sendButtonPressed(_ sender: UIButton) {
+    }
+    
+}
+
