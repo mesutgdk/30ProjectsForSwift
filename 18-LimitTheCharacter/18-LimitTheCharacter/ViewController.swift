07dfce35576336d66f48360df3159e679fb9acaa
Mesut GEDİK <112901255+mesutgdk@users.noreply.github.com>
2023-04-05 08:33:36 +0300

    Add files via upload

--- 18-LimitTheCharacter/18-LimitTheCharacter/ViewController.swift
+++ 18-LimitTheCharacter/18-LimitTheCharacter/ViewController.swift
@@ -11,17 +11,38 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var imageView: UIImageView!
     
+    @IBOutlet weak var charecterCountLabel: UILabel!
     @IBOutlet weak var textView: UITextView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        charecterCountLabel.text = "\(140)"
+        textView.delegate = self
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        textView.isFirstResponder
+
     }
 
     @IBAction func backButtonPressed(_ sender: UIButton) {
     }
     @IBAction func sendButtonPressed(_ sender: UIButton) {
     }
+    
+
+}
+extension ViewController: UITextViewDelegate {
+        func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+            var newText = textView.text!
+            newText.removeAll { (character) -> Bool in
+                return character == " " || character == "\n"
+            }
+            let numberChar = newText.count + text.count
+            charecterCountLabel.text = "\(140 - numberChar)"
+            return numberChar < 140
+        }
+    
     
 }
 
