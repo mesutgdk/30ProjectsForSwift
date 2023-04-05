29d62dd634d2eda9478521d01c74b3357f1a423e
Mesut GEDİK <112901255+mesutgdk@users.noreply.github.com>
2023-04-05 16:10:23 +0300

    Add files via upload

--- 18-LimitTheCharacter/18-LimitTheCharacter/ViewController.swift
+++ 18-LimitTheCharacter/18-LimitTheCharacter/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var imageView: UIImageView!
-    
+    @IBOutlet weak var bottomView: UIView!
     @IBOutlet weak var charecterCountLabel: UILabel!
     @IBOutlet weak var textView: UITextView!
     
@@ -18,20 +18,46 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         charecterCountLabel.text = "\(140)"
         textView.delegate = self
+        NotificationCenter.default.addObserver(self, selector:#selector(ViewController.keyboardWillShow(notification:)), name:UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector:#selector(ViewController.keyboardWillHide(notification:)), name:UIResponder.keyboardWillHideNotification, object: nil)
+        print(self.bottomView.frame.origin.y)
+        
     }
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        textView.isFirstResponder
-
-    }
+    
 
     @IBAction func backButtonPressed(_ sender: UIButton) {
     }
     @IBAction func sendButtonPressed(_ sender: UIButton) {
     }
-    
-
 }
+// MARK: - Move View when Keyboard Appear-Hide
+extension ViewController {
+   
+   @objc func keyboardWillShow(notification: NSNotification) {
+       let userInfo  = notification.userInfo
+       let keyBoardBounds = (userInfo![UIResponder.keyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
+       let duration = (userInfo![UIResponder.keyboardAnimationDurationUserInfoKey] as! NSNumber).doubleValue
+       let deltaY = keyBoardBounds.height
+       let animations = {
+           self.bottomView.transform = CGAffineTransform(translationX: 0, y: -deltaY)
+       }
+
+       if duration > 0 {
+           
+           UIView.animate(withDuration: duration, delay: 0, options:[.beginFromCurrentState, .curveLinear], animations: animations, completion: nil)
+       }else {
+           animations()
+       }
+       
+   
+    }
+    @objc func keyboardWillHide(notification: NSNotification) {
+        self.bottomView.transform = .identity
+        
+        
+    }
+}
+// MARK: -UITextViewDelegate
 extension ViewController: UITextViewDelegate {
         func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
             var newText = textView.text!
