422131f2129d8054d1414f5117dc78d9a6c91a24
Mesut GEDİK <112901255+mesutgdk@users.noreply.github.com>
2023-04-06 20:29:11 +0300

    Add files via upload

--- 19-PullToRef/19-PullToRef/RefreshViewController.swift
+++ 19-PullToRef/19-PullToRef/RefreshViewController.swift
@@ -9,9 +9,44 @@ import UIKit
 
 class RefreshViewController: UITableViewController {
 
+    var carModels: [String] = ["Alfa-Romeo",
+                               "BMV",
+                               "Dacia",
+                               "Fiat",
+                               "Ford",
+                               "Honda",
+                               "Jaguar",
+                               "Kia",
+                               "Mercedes",
+                               "Opel",
+                               "Renault",
+                               "Seat",
+                               "Skoda",
+                               "Toyota",
+                               "Volvo"]
+    let reusableCell = "refreshCell"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.refreshControl = UIRefreshControl()
+        tableView.refreshControl?
+        
+    }
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return carModels.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: reusableCell, for: indexPath)
+        cell.textLabel?.text = carModels[indexPath.row]
+        
+        return cell
+    }
+    @objc func callOfRefresh (){
+        DispatchQueue.main.async {
+            self.tableView.refreshControl?.endRefreshing()
+            self.tableView.reloadData()
+        }
     }
 
 
