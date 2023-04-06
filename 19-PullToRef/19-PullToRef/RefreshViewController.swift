e2c9f5f924ef63b3fda9746718b21aa71148ee1c
Mesut GEDİK <112901255+mesutgdk@users.noreply.github.com>
2023-04-06 19:57:32 +0300

    Add files via upload

---
+++ 19-PullToRef/19-PullToRef/RefreshViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  19-PullToRef
+//
+//  Created by Mesut Gedik on 6.04.2023.
+//
+
+import UIKit
+
+class RefreshViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
