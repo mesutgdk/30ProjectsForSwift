8ead7db8a5f88dfdbaa78c7b47c81458a314c3ac
Mesut GEDİK <112901255+mesutgdk@users.noreply.github.com>
2023-04-07 11:38:20 +0300

    Add files via upload

---
+++ 19-PullToRef/19-PullToRef/Constants.swift
@@ -0,0 +1,32 @@
+//
+//  Constants.swift
+//  19-PullToRef
+//
+//  Created by Mesut Gedik on 7.04.2023.
+//
+
+import Foundation
+
+struct M {
+    static let reusableCell = "refreshCell"
+    
+    static let whatIDid: [String] = ["01-SimpleTimer",
+                                     "02-Change Font Ya",
+                                     "03-PlayLocalVideo",
+                                     "04-SnapyMenu",
+                                     "05-CarouselEffect",
+                                     "06-FindMyLocation",
+                                     "07-PullToRefresh",
+                                     "08-Colorization",
+                                     "09-ImageScrollerEffeckt",
+                                     "10-VideoBackground",
+                                     "11-ClearTableView",
+                                     "12-LoginAnimation",
+                                     "13-AnimeteTable",
+                                     "14-SlotMachine",
+                                     "15-AnimatedSplash",
+                                     "16-SlideMenue",
+                                     "17-TumblrMenu",
+                                     "18-LimitTheCharacter",
+                                     "19-PullToRef"]
+}
--- 19-PullToRef/19-PullToRef/RefreshViewController.swift
+++ 19-PullToRef/19-PullToRef/RefreshViewController.swift
@@ -8,23 +8,6 @@
 import UIKit
 
 class RefreshViewController: UITableViewController {
-
-    var carModels: [String] = ["Alfa-Romeo",
-                               "BMV",
-                               "Dacia",
-                               "Fiat",
-                               "Ford",
-                               "Honda",
-                               "Jaguar",
-                               "Kia",
-                               "Mercedes",
-                               "Opel",
-                               "Renault",
-                               "Seat",
-                               "Skoda",
-                               "Toyota",
-                               "Volvo"]
-    let reusableCell = "refreshCell"
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,12 +16,12 @@ class RefreshViewController: UITableViewController {
         
     }
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return carModels.count
+        return M.whatIDid.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: reusableCell, for: indexPath)
-        cell.textLabel?.text = carModels[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: M.reusableCell, for: indexPath)
+        cell.textLabel?.text = M.whatIDid[indexPath.row]
         
         return cell
     }
