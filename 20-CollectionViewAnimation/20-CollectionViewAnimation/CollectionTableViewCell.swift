7ab626efa91638d7db378ab4a934e6afcd6743a9
Mesut GEDİK <112901255+mesutgdk@users.noreply.github.com>
2023-04-09 17:26:40 +0300

    Add files via upload

---
+++ 20-CollectionViewAnimation/20-CollectionViewAnimation/CollectionTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CollectionTableViewCell.swift
+//  20-CollectionViewAnimation
+//
+//  Created by Mesut Gedik on 9.04.2023.
+//
+
+import UIKit
+
+class CollectionTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ 20-CollectionViewAnimation/20-CollectionViewAnimation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  20-CollectionViewAnimation
+//
+//  Created by Mesut Gedik on 9.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
