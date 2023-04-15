a8112cb373eb84c5d188ca8224d17b85d539bb26
Mesut GEDİK <112901255+mesutgdk@users.noreply.github.com>
2023-04-15 18:51:02 +0300

    Add files via upload

---
+++ 22-3D Touch Quick Action/22-3D Touch Quick Action/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  22-3D Touch Quick Action
+//
+//  Created by Mesut Gedik on 15.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+//    battary clock is light now **
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
