7d9f57245b8ffa8426afbe53c21b023d87cfb31d
Mesut GEDİK <112901255+mesutgdk@users.noreply.github.com>
2023-04-17 12:05:52 +0300

    Add files via upload

---
+++ 22-3D Touch Quick Action/22-3D Touch Quick Action/RunViewController.swift
@@ -0,0 +1,29 @@
+//
+//  RunViewController.swift
+//  22-3D Touch Quick Action
+//
+//  Created by Mesut Gedik on 17.04.2023.
+//
+
+import UIKit
+
+class RunViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
