ed931366e4a18a901d4c3afc439f472b2e74c775
Mesut GEDİK <112901255+mesutgdk@users.noreply.github.com>
2023-04-19 11:59:53 +0300

    Add files via upload

---
+++ 23-Side Menu/23-Side Menu/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  23-Side Menu
+//
+//  Created by Mesut Gedik on 18.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
