ac0a982916cc44183dca217909049f82bfd20c43
Mesut GEDİK <112901255+mesutgdk@users.noreply.github.com>
2023-04-20 15:06:34 +0300

    Add files via upload

---
+++ 23-Side Menu/23-Side Menu/MenuTableViewController.swift
@@ -0,0 +1,53 @@
+//
+//  MenuTableViewController.swift
+//  23-Side Menu
+//
+//  Created by Mesut Gedik on 19.04.2023.
+//
+
+import UIKit
+
+protocol MenuTableViewDelegate {
+    func didSelectMenuItem(name: String)
+}
+
+class MenuTableViewController: UITableViewController {
+
+    var delegate: MenuTableViewDelegate?
+    
+    private let menuItems: [String] = [
+    "KADIN","ERKEK","ÇOCUK"]
+    
+    private let mcell = "menuCell"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: mcell)
+        tableView.backgroundColor = UIColor(red: 33/255, green: 33/255, blue: 33/255, alpha: 1)
+        view.backgroundColor = .darkGray
+    }
+
+    // MARK: - Table view data source
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return menuItems.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: mcell, for: indexPath)
+        
+        cell.textLabel?.text = menuItems[indexPath.row]
+        cell.backgroundColor = .clear
+        cell.textLabel?.textColor = .white
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        let selectedItem = menuItems[indexPath.row]
+        delegate?.didSelectMenuItem(name: selectedItem)
+    }
+    
+    
+
+}
--- 23-Side Menu/23-Side Menu/ViewController.swift
+++ 23-Side Menu/23-Side Menu/ViewController.swift
@@ -8,20 +8,41 @@
 import UIKit
 import SideMenu
 
-class ViewController: UIViewController {
-    
-    private let sideMenu = SideMenuNavigationController(rootViewController: UIViewController())
+class ViewController: UIViewController, MenuTableViewDelegate {
+   
+   
+//    private let sideMenu = SideMenuNavigationController(rootViewController: MenuTableViewController())
+    private var sideMenu: SideMenuNavigationController?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        sideMenu.leftSide = true
+        let menu = MenuTableViewController()
+        menu.delegate = self
+        sideMenu = SideMenuNavigationController(rootViewController: menu)
+        sideMenu?.leftSide = true
+//        sideMenu?.delegate = self
+        
         SideMenuManager.default.leftMenuNavigationController = sideMenu
         SideMenuManager.default.addPanGestureToPresent(toView: view)
         
     }
     @IBAction func menuButtonPressed (){
-        present(sideMenu, animated: true)
+        present(sideMenu!, animated: true)
         
     }
+    func didSelectMenuItem(name: String) {
+        sideMenu?.dismiss(animated: true,completion: { [weak self] in
+            if name == "KADIN" {
+                self?.view.backgroundColor = .white
+            } else if name == "ERKEK"{
+                self?.view.backgroundColor = .yellow
+                
+            }else if name == "ÇOCUK" {
+                self!.view.backgroundColor = .red
+                
+            }
+        })
+    }
+    
 }
 
