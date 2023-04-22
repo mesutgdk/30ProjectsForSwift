46ff9738cb10075eee0e1bb1e0bea4dcca1efe34
Mesut GEDİK <112901255+mesutgdk@users.noreply.github.com>
2023-04-22 17:40:42 +0300

    Add files via upload

---
+++ 24-MosaicLayOut/24-MosaicLayOut/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  24-MosaicLayOut
+//
+//  Created by Mesut Gedik on 22.04.2023.
+//
+
+import UIKit
+
+class ViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
