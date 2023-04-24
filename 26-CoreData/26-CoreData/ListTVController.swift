871be3f0c7f251df302b0a502d6b6ef7fc081fb6
Mesut GEDİK <112901255+mesutgdk@users.noreply.github.com>
2023-04-24 19:35:45 +0300

    Add files via upload

--- 26-CoreData/26-CoreData/ListTVController.swift
+++ 26-CoreData/26-CoreData/ListTVController.swift
@@ -8,24 +8,23 @@
 import UIKit
 
 class ListTVController: UITableViewController {
-
+    
     let cCell = "listCell"
     
-    let listData: [String] = [""]
+    let listData : [String] = [""]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return listData.count
     }
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: cCell, for: indexPath)
         cell.textLabel?.text = listData[indexPath.row]
+        
         return cell
     }
-    
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
         
     }
