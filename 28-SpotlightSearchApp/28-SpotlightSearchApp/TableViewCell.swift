917a528e4632fb3a467eb09ffa0dbe2a86a88768
Mesut GEDİK <112901255+mesutgdk@users.noreply.github.com>
2023-04-27 23:02:06 +0300

    Add files via upload

---
+++ 28-SpotlightSearchApp/28-SpotlightSearchApp/TableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TableViewCell.swift
+//  28-SpotlightSearchApp
+//
+//  Created by Mesut Gedik on 27.04.2023.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ 28-SpotlightSearchApp/28-SpotlightSearchApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  28-SpotlightSearchApp
+//
+//  Created by Mesut Gedik on 27.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
