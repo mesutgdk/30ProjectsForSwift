4ff81a6664d9af6f6680bbd73e98b6ae03153358
Mesut GEDİK <112901255+mesutgdk@users.noreply.github.com>
2023-04-27 18:10:36 +0300

    Add files via upload

--- 27-TabBarApp/27-TabBarApp/ThirdViewController.swift
+++ 27-TabBarApp/27-TabBarApp/ThirdViewController.swift
@@ -9,11 +9,29 @@ import UIKit
 
 class ThirdViewController: UIViewController {
 
+    @IBOutlet weak var myAccountImage: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        resetImage()
+        }
+    override func viewWillAppear(_ animated: Bool) {
+        
+            super.viewDidAppear(animated)
+            
+        UIView.animate(withDuration: 0.8, delay: 0.1, usingSpringWithDamping: 0.7, initialSpringVelocity: 1, options: .curveEaseIn, animations: { [self] in
+                myAccountImage.transform = CGAffineTransform(scaleX: 1, y: 1)
+                myAccountImage.alpha = 1
+                
+                }, completion: nil )
     }
-
-
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        resetImage()
+    }
+    
+    func resetImage (){
+        myAccountImage.alpha = 0
+        myAccountImage.transform = CGAffineTransform(scaleX: 0.5, y: 0.5)
+    }
 }
 
