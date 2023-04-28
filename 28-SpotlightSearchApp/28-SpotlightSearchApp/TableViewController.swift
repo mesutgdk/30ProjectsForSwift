393b6195d2b69d75f54e6800b6fad093217b9e80
Mesut GEDİK <112901255+mesutgdk@users.noreply.github.com>
2023-04-28 21:16:24 +0300

    Add files via upload

--- 28-SpotlightSearchApp/28-SpotlightSearchApp/TableViewController.swift
+++ 28-SpotlightSearchApp/28-SpotlightSearchApp/TableViewController.swift
@@ -9,28 +9,46 @@ import UIKit
 
 class TableViewController: UITableViewController {
 
-        let cCell = "tableCell"
+    let cCell = "tableCell"
+    let movieData = MovieData()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
 
     }
 
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return movieData.movieFile.count
     }
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 150
     }
 
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cCell, for: indexPath) as! TableViewCell
+        let roadPath = movieData.movieFile[indexPath.row]
+        
+        cell.movieImage.image = UIImage(named: roadPath.imageName)
+        cell.movieType?.text = roadPath.movieType
+        cell.movieTitle?.text = roadPath.movieTitle
+        cell.movieStars?.text = roadPath.movieStars
+        cell.movieRating?.text = roadPath.movieRating
+        cell.movieDirector?.text = roadPath.movieDirector
+        cell.movieDescription?.text = roadPath.movieDescription
+        cell.movieRating.layer.masksToBounds = true
+        cell.movieRating.layer.cornerRadius = 10
+        
+   
+        
+        
+        return cell
+    }
     /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
