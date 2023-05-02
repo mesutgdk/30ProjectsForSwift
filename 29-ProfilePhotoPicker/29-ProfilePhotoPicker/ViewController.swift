4b1ed09f2235fe11249a6a6bf3902bc64068876f
Mesut GEDİK <112901255+mesutgdk@users.noreply.github.com>
2023-05-02 20:00:11 +0300

    Add files via upload

---
+++ 29-ProfilePhotoPicker/29-ProfilePhotoPicker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  29-ProfilePhotoPicker
+//
+//  Created by Mesut Gedik on 2.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
