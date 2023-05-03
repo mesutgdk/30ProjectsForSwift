eb19f767ac68a779aa14bd75655ad5de137ee4ef
Mesut GEDİK <112901255+mesutgdk@users.noreply.github.com>
2023-05-03 19:12:05 +0300

    Add files via upload

---
+++ 30-WikiFaceDetection/30-WikiFaceDetection/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  30-WikiFaceDetection
+//
+//  Created by Mesut Gedik on 3.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
